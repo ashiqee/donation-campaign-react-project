5fff218bcc4fce7fb1880fa9674a90dfcae033ca
ashiq73 <ashiq.buet73@gmail.com>
2023-09-25 03:28:06 +0600

    Donation Card Details page and load details data

--- src/pages/Donation/DonationsDetails/DonationDetail.jsx
+++ src/pages/Donation/DonationsDetails/DonationDetail.jsx
@@ -1,9 +1,26 @@
-import { useLoaderData } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLoaderData, useParams } from "react-router-dom";
+import DonationsDetails from "./DonationsDetails";
 
 const DonationDetail = () => {
-  const donation = useLoaderData();
+  const [donationData, setDonationData] = useState({});
+  const donations = useLoaderData();
   //   console.log(donation);
-  return <div></div>;
+
+  const { id } = useParams();
+  const idInt = parseInt(id);
+
+  useEffect(() => {
+    const findDonation = donations?.find((donation) => donation.id === idInt);
+    setDonationData(findDonation);
+  }, [idInt, donations]);
+
+  //   console.log(donationData);
+  return (
+    <div>
+      <DonationsDetails donationData={donationData} />
+    </div>
+  );
 };
 
 export default DonationDetail;
--- src/pages/Donation/DonationsDetails/DonationsDetails.jsx
+++ src/pages/Donation/DonationsDetails/DonationsDetails.jsx
@@ -1,5 +1,40 @@
-const DonationsDetails = () => {
-  return <div></div>;
+const DonationsDetails = ({ donationData }) => {
+  const {
+    id,
+    Title,
+    Description_img,
+    Description,
+    Category,
+    Button_bg,
+    Text_bg,
+    Card_bg,
+    Picture,
+    Price,
+  } = donationData;
+
+  return (
+    <div className="max-w-7xl mx-auto mt-10">
+      <figure className="relative">
+        <div href="#">
+          <img
+            className="rounded-lg h-full w-full md:h-[700px] lg:h-[80vh]"
+            src={Description_img}
+            alt="image description"
+          />
+        </div>
+        <div className="relative bottom-24 rounded-lg opacity-50 bg-gray-700 w-full h-24 "></div>
+        <figcaption className="absolute px-10  text-lg text-white bottom-32">
+          <button
+            className="p-2 rounded-lg"
+            style={{ backgroundColor: Text_bg }}>
+            Donate ${Price}
+          </button>
+        </figcaption>
+      </figure>
+      <h2 className="text-3xl font-bold mt-0 mb-5">{Title}</h2>
+      <p className="text-justify font-normal mb-10">{Description}</p>
+    </div>
+  );
 };
 
 export default DonationsDetails;
